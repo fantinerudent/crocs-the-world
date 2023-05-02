f9fb91419ec894a348122622f605918b0de4a731
Fantine <fantine.rudent@hotmail.fr>
2023-05-02 12:26:42 +0200

    add post route

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import chalk from "chalk";
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 const connection = require("./db.cjs");
-const serverPort =  3000;
+const serverPort = 3000;
 const app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(serverPort, () => {
   console.log(chalk.blue.bgRed.bold(" le serveur est lancé !!!!"));
@@ -25,6 +28,29 @@ app.get("/croc", (req, res) => {
     .query("SELECT * FROM croc")
     .then(([results]) => {
       res.json(results);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving products from db.");
+    });
+});
+
+app.post("/croc", (req, res) => {
+  console.log(req.body);
+  const { name, price, description, imgUrl, color } = req.body;
+  const SQL_query =
+    "INSERT INTO croc (name, price, description, img_url, color) VALUES (?, ?, ?, ?, ?)";
+  connection
+    .promise()
+    .query(
+      SQL_query, [name, price, description, imgUrl, color]
+    )
+    .then(([results]) => {
+      if (results.affectedRows === 1) {
+        res.status(201).json({ message: "crocn added" });
+      } else {
+        res.status(500).json({ message: "error" });
+      }
     })
     .catch((err) => {
       console.error(err);
